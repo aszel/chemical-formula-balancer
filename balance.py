9059883b28101901cf6a1d448d7a271800403296
Martin Becker <mbr@dasburo.com>
2019-12-23 00:54:19 +0100

    Catching non existent species. This can happen when a speciesref points to a species by id but this does not exist

--- balance.py
+++ balance.py
@@ -4,8 +4,8 @@ import re
 
 class Balancer:
 
-    file_name = "Supporting Information File S1_Jcurcas_model.xml"
-    #file_name = "test-data.xml"
+    #file_name = "Supporting Information File S1_Jcurcas_model.xml"
+    file_name = "test-data.xml"
     node_name_list_of_species = "listOfSpecies"
     node_name_list_of_reactions = "listOfReactions"
     node_name_reactants = "listOfReactants"
@@ -104,6 +104,12 @@ class Balancer:
         except ValueError:
             return False
 
+    def species_exists(self, id, species):
+        if id in species:
+            return True
+        else:
+            return False
+
     def get_combined_chemical_formula(self, species_dict, dict_elements):
         my_formulas_list = []
         for id, coefficient in dict_elements.items():
@@ -148,10 +154,10 @@ class Balancer:
     a none-integer value as coefficient or their referrenced species has no
     chemical formula
     """
-    def remove_unusable_elements(self, elements_dict, species_without_chemical_formula):
+    def remove_unusable_elements(self, elements_dict, species_without_chemical_formula, species):
         my_dict = {}
         for id, coefficient in elements_dict.items():
-            if not id in species_without_chemical_formula and self.is_integer(coefficient):
+            if not id in species_without_chemical_formula and self.is_integer(coefficient) and self.species_exists(id, species):
                 my_dict.update({id : coefficient})
         return my_dict
 
@@ -160,6 +166,38 @@ b = Balancer()
 full_file_name = b.get_full_file_name()
 model = b.get_model_node(full_file_name)
 species, species_without_chemical_formula = b.get_species(model)
+
+reactions = b.get_reactions(model)
+for reaction_id, (list_of_reactants, list_of_products) in reactions.items():
+    dict_reactants = b.get_species_references(list_of_reactants)
+    reactants_dict_updated = b.remove_unusable_elements(dict_reactants, species_without_chemical_formula, species)
+
+    products_dict = b.get_species_references(list_of_products)
+    products_dict_updated = b.remove_unusable_elements(products_dict, species_without_chemical_formula, species)
+
+    combined_reactants_formula = b.get_combined_chemical_formula(species, reactants_dict_updated)
+    combined_products_formula = b.get_combined_chemical_formula(species, products_dict_updated)
+
+    print("------------")
+    print("Reaction ID                       -> ", reaction_id)
+    print("Reactants [speciesId,coefficient] -> ", reactants_dict_updated)
+    print("Products  [speciesId,coefficient] -> ", products_dict_updated)
+    print("Reactants chem. formulas combined -> ", combined_reactants_formula)
+    print("Products chem. formulas combined  -> ", combined_products_formula)
+    added, removed, modified, same = b.compare(combined_reactants_formula, combined_products_formula)
+    print("Comparison")
+    print("Elemements in Reactants only      -> ", added)
+    print("Elemements in Products only       -> ", removed)
+    print("Differences                       -> ", modified)
+    print("Equal elements                    -> ", same)
+
+print("------------")
+print("Summary")
+print("Number of reactions: ", len(reactions))
+print("Skipped species without chemical formula: ", species_without_chemical_formula)
+
+
+
 
 # DO NOT DELETE - this is for testing
 #b.calculate_chemical_formula(species, "M_aicar_d", 2)
@@ -170,29 +208,3 @@ species, species_without_chemical_formula = b.get_species(model)
 #some_bad_species_dict = {'M_Biomass_c': ''}
 #result = b.remove_unusable_elements(some_bad_reactants_dict, some_bad_species_dict)
 #print(result)
-
-reactions = b.get_reactions(model)
-for reaction_id, (list_of_reactants, list_of_products) in reactions.items():
-    print("------------")
-    print("Reaction  -> ", reaction_id)
-
-    dict_reactants = b.get_species_references(list_of_reactants)
-    reactants_dict_updated = b.remove_unusable_elements(dict_reactants, species_without_chemical_formula)
-
-    products_dict = b.get_species_references(list_of_products)
-    products_dict_updated = b.remove_unusable_elements(products_dict, species_without_chemical_formula)
-
-    combined_reactants_formula = b.get_combined_chemical_formula(species, reactants_dict_updated)
-    combined_products_formula = b.get_combined_chemical_formula(species, products_dict_updated)
-
-    print("Reactants -> ", reactants_dict_updated)
-    print("Products  -> ", products_dict_updated)
-    print("Reactants Combined -> ", combined_reactants_formula)
-    print("Products Combined  -> ", combined_products_formula)
-    added, removed, modified, same = b.compare(combined_reactants_formula, combined_products_formula)
-    print("Comparison -> \nElemements in Reactants only: ", added, "\nElemements in Products only: ", removed, "\nDifferences: ", modified, "\nEqual elements: ", same)
-
-print("------------")
-print("Summary")
-print("Number of reactions: ", len(reactions))
-print("Skipped species without chemical formula: ", species_without_chemical_formula)
