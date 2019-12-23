2b20555d6a85bf19c611b98fa30fa8a3b24803c7
Martin Becker <mbr@dasburo.com>
2019-12-23 17:34:41 +0100

    Changed programme structure to a somewhat more mature one

--- balancer/balance.py
+++ balancer/balance.py
@@ -4,7 +4,8 @@ import re
 
 class Balancer:
 
-    file_name = "Supporting Information File S1_Jcurcas_model.xml"
+    file_name = ""
+    #file_name = "Supporting Information File S1_Jcurcas_model.xml"
     #file_name = "test-data.xml"
     node_name_list_of_species = "listOfSpecies"
     node_name_list_of_reactions = "listOfReactions"
@@ -17,6 +18,9 @@ class Balancer:
     species_ref_attribute_coefficient = "stoichiometry"
     species_attribute_id = "id"
     species_attribute_chemical_formula = "chemicalFormula"
+
+    def __init__(self, file_name):
+        self.file_name = file_name
 
     def get_full_file_name(self):
         full_file_name = os.path.abspath(os.path.join("data", self.file_name))
@@ -169,7 +173,7 @@ class Balancer:
         print("Products  [speciesId,coefficient] -> ", products_dict_updated)
         print("Reactants chem. formulas combined -> ", combined_reactants_formula)
         print("Products chem. formulas combined  -> ", combined_products_formula)
-        added, removed, modified, same = b.compare(combined_reactants_formula, combined_products_formula)
+        added, removed, modified, same = self.compare(combined_reactants_formula, combined_products_formula)
         if len(added) == 0:
             added = ""
         if len(removed) == 0:
@@ -189,35 +193,3 @@ class Balancer:
         print("Summary")
         print("Number of reactions: ", len(reactions))
         print("Skipped species without chemical formula: ", species_without_chemical_formula)
-
-
-b = Balancer()
-full_file_name = b.get_full_file_name()
-model = b.get_model_node(full_file_name)
-species, species_without_chemical_formula = b.get_species(model)
-reactions = b.get_reactions(model)
-
-for reaction_id, (list_of_reactants, list_of_products) in reactions.items():
-    dict_reactants = b.get_species_references(list_of_reactants)
-    reactants_dict_updated = b.remove_unusable_elements(dict_reactants, species_without_chemical_formula, species)
-
-    products_dict = b.get_species_references(list_of_products)
-    products_dict_updated = b.remove_unusable_elements(products_dict, species_without_chemical_formula, species)
-
-    combined_reactants_formula = b.get_combined_chemical_formula(species, reactants_dict_updated)
-    combined_products_formula = b.get_combined_chemical_formula(species, products_dict_updated)
-
-    b.print_status(reaction_id, reactants_dict_updated, products_dict_updated,
-        combined_reactants_formula, combined_products_formula)
-
-b.print_summary(reactions, species_without_chemical_formula)
-
-# DO NOT DELETE - this is for testing
-#b.calculate_chemical_formula(species, "M_aicar_d", 2)
-#test_formula_list = [{'C': 2, 'H': 1, 'N': 8}, {'C': 3, 'H': 2, 'X': 8}]
-#combined_formulas = b.combine_chemical_formulas(test_formula_list)
-#print(combined_formulas)
-#some_bad_reactants_dict = {'test_1': '2', 'test_2': '1.1'}
-#some_bad_species_dict = {'M_Biomass_c': ''}
-#result = b.remove_unusable_elements(some_bad_reactants_dict, some_bad_species_dict)
-#print(result)
